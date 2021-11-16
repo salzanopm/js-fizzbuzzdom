2383b6a340c264c0e3d9bfe20d297aa577c6a1ec
salzanopm <salzanopm@email.com>
2021-11-16 17:05:25 +0100

    milestone-3

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -5,41 +5,50 @@
 
 // inizio ciclo
 for(let i = 0; i <= 100; i++) {
+
+    // dichiaro variabile "multipleFizzBuzz"
     let multipleFizzBuzz;
-    // dichiaro variabile "multipleFizzBuzz" 
-    let colore;
+
+    // dichiaro variabile "color"
+    let color;
+
+    // inizio ciclo condizioni
+    // 1a condizione
     if (( i % 3  === 0 )&&( i % 5 === 0 )){ // se divisibile per 3 e 5 e senza resto
-        multipleFizzBuzz = 'FizzBuzz'
-        colore='red'
-        // colore rosso
+
+        multipleFizzBuzz = 'FizzBuzz'       // attribuisco il valore 'FizzBuzz' alla stringa multipleFizzBuzz
+        color='red'                         // attribuisco il valore 'red' alla stringa color
+
+    // 2a condizione
     } else if ( i % 3 === 0 ) {             // oppure se divisibile per 3 e senza resto
-        multipleFizzBuzz = 'Fizz' 
-        // colore verde   
-        colore='green'                     // attribuisco valore stringa Fizz
+
+        multipleFizzBuzz = 'Fizz'           // attribuisco il valore 'Fizz' alla stringa multipleFizzBuzz               
+        color='green'                       // attribuisco il valore 'green' alla stringa color 
+
+    // 3a condizione
     } else if( i % 5  === 0 ) {             // oppure se divisibile per 5 e senza resto
-        multipleFizzBuzz = 'Buzz'
-        colore='yellow' 
-        // colore giallo                       // attribuisco valore stringa Buzz
-    } else {                                // oppure se divisibile per 5 e senza resto
-        multipleFizzBuzz = i  
-        // nessun colore              // attribuisco valore stringa Buzz
+
+        multipleFizzBuzz = 'Buzz'           // attribuisco il valore 'Buzz' alla stringa multipleFizzBuzz
+        color='yellow'                      // attribuisco il valore 'yellow' alla stringa color
+
+    // ultima condizione           
+    } else {                                // oppure
+
+        multipleFizzBuzz = i                // il restante lo associo a multipleFizzBuzz
+
     }
 
-    // stampo
     // dichiaro che variabile newBox crei div con classe css .box dentro scritto valore multipleFizzBuzz
     const newBox = `
-    <div class="box ${colore}">${multipleFizzBuzz}</div>
+    <div class="box ${color}">${multipleFizzBuzz}</div>
     
     `;
     // dichiaro variabile boxesContainer associato al mio container
     const boxesContainer = document.querySelector('.container');
     // stampo dentro al container 
     boxesContainer.innerHTML += newBox;
-    // parto con la condizione
 
 
     
 }
  
-// const element = document.querySelector('.box');
-//         element.classList.add('red'); // attribuisco valore stringa FizzBuzz
